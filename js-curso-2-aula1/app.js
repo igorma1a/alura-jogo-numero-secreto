f98dbaf3f34310b56eb6478cb05105d7f2046073
Igor Maia <igormaiadejesus>
2025-02-07 11:04:45 -0300

    first commit

---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,91 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 100;
+let numeroSecreto = gerarNumeroAleatorio();
+let contadorTentativas = 1;
+
+exibirMensagemInicial();
+
+
+// Função alterar texto na tela
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+
+// Função para exibir a mensagem inicial
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto!');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}.`);
+}
+
+
+// Função verificar acerto
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        let palavraTentativa = contadorTentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto em ${contadorTentativas} ${palavraTentativa}!`;
+
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', mensagemTentativas);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    } else {
+
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor.');
+
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior.');
+
+        }
+
+        contadorTentativas++;
+        limparCampo();
+    }
+}
+
+
+// Função gerar número aleatório
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    // Se número escolhido está includo no array, chama a função novamente
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+
+    }
+}
+
+
+// Função limpa o valor do input (quando erra)
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+
+// Função reiniciar o jogo
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    contadorTentativas = 1;
+
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
+}
---
+++ js-curso-2-desafio_1/app.js
@@ -0,0 +1,116 @@
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Hora do Desafio.';
+
+
+
+// function consoleBtn() {
+//     console.log('O botão foi clicado.');
+// }
+
+
+
+// function alertBtn() {
+//     alert('Eu amo JS.');
+// }
+
+
+
+// function promptBtn() {
+//     let cidade = prompt('Digite o nome de uma cidade: ');
+//     alert(`Estive em ${cidade} e lembrei de você!`);
+// }
+
+
+
+// function somaBtn() {
+//     let numero1 = parseInt(prompt('Digite um número inteiro: '));
+//     let numero2 = parseInt(prompt('Digite um segundo número inteiro: '));
+
+//     alert(`O resultado da soma dos números é ${numero1 + numero2}`);
+// }
+
+
+
+
+// function olaMundo() {
+//     console.log('Olá, Mundo!');
+// }
+
+// olaMundo();
+
+
+
+// function olaMundo2(nome) {
+//     console.log(`Olá, ${nome}!`);
+// }
+
+// olaMundo2('Igor');
+
+
+
+// function dobro(numero) {
+//     return numero*2;
+// }
+
+// console.log(dobro(2));
+
+
+
+// function media(numero1, numero2, numero3) {
+//     let numeroMedia = (numero1 + numero2 + numero3) / 3
+//     return numeroMedia;
+// }
+
+// console.log(media(4, 6, 8));
+
+
+
+// function maior(numero1, numero2) {
+//     if (numero1 > numero2) {
+//         return numero1;
+
+//     } else {
+//         return numero2;
+//     }
+// }
+
+// console.log(maior(7, 2));
+
+
+
+// function multiplicacao(numero) {
+//     return numero * numero;
+// }
+
+// console.log(multiplicacao(5));
+
+
+
+// function calcularIMC(altura, peso) {
+//     return peso / altura;
+// }
+
+// console.log(calcularIMC(1.8, 115));
+
+
+
+// function fatorial(numero) {
+//     resultado = numero - 1
+
+//     return
+// }
+
+
+
+let listaGenerica = [];
+
+let liguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Python'];
+
+liguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(liguagensDeProgramacao);
+
+let nomes = ['Igor', 'Janice', 'Antonio'];
+
+console.log(nomes[0]);
+console.log(nomes[1]);
+console.log(nomes[2]);
